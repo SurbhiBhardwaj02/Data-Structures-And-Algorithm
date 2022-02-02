31f9d8cc8338f4a89d48a2cef0b6bfc818926e39
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2022-02-02 10:23:50 +0530

    Program for all operations on singly Linked List

---
+++ MenuDriven.java
@@ -0,0 +1,62 @@
+/*import java.util.Scanner;
+
+class Node1
+{
+    int data;
+    Node next;
+    public Node1(int data)
+    {
+        this.data=data;
+    }
+
+}
+public class MenuDriven {
+
+    class Node1
+    {
+        int data;
+        Node next;
+        public Node1(int data)
+        {
+            this.data=data;
+        }
+
+    }
+    public static void main(String[] args) {
+
+
+        int i;
+        int x = 1;
+
+        Scanner s = new Scanner(System.in);
+
+        while (x == 1) {
+            System.out.println("Enter your Choice:");
+            System.out.println("1.InsertAtBeginning");
+            System.out.println("2.InsertAtLast");
+            System.out.println("3.InsertAtPosition");
+            System.out.println("1.DeleteAtBeginning");
+            System.out.println("2.DeleteAtLast");
+            System.out.println("3.DeleteAtPosition");
+            i = s.nextInt();
+            switch (i) {
+                Node1 head;
+                Node1 newnode=
+                case 1:
+
+                    if (head == null) {
+                        head = newnode;
+                    } else {
+                        newnode.next = head;
+                        head = newnode;
+                    }
+                    System.out.println("Press 1 to continue ,Press 0 for Exit:");
+                    x = s.nextInt();
+                    break;
+                case 2:
+
+
+            }
+        }
+    }
+}*/
